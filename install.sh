36900727c3ecfc487c7be1549cb1fff76a820503
The-Jonsey <louis@thejonsey.com>
2019-09-19 16:21:24 +0100

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,23 +1,40 @@
 #!/usr/bin/env bash
 
-which vim > /dev/null
+function install() {
 
-if [ $? -ne 0 ]; then
-    echo "Please install vim to continue"
-    exit 1
+    which vim > /dev/null
+
+    if [ $? -ne 0 ]; then
+        echo "Please install vim to continue"
+        exit 1
+    fi
+
+
+    ln -s $(pwd)/vim ~/.vim
+    ln vimrc ~/.vimrc
+    ln vimrc.bundles ~/.vimrc.bundles
+
+    cp vimrc.local ~/.vimrc.local
+    cp vimrc.bundles.local ~/.vimrc.bundles.local
+
+    git config --global core.autocrlf input
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null
+
+    $(which vim) -c "PluginInstall!" -c "q" -c "q"
+
+    echo "And you're done! Have fun with your new and improved vim!"
+
+}
+
+function uninstall() {
+    rm -rf ~/.vim ~/.vimrc ~/.vimrc.bundles ~/.vimrc.local ~/.vimrc.bundles.local
+    echo "vim-up is uninstalled, there may be some files that need to be removed manually"
+}
+
+if [ $1 == "install" ]; then
+    install
+elif [ $1 == "uninstall" ]; then
+    uninstall
+else
+    install
 fi
-
-
-ln -s $(pwd)/vim ~/.vim
-ln vimrc ~/.vimrc
-ln vimrc.bundles ~/.vimrc.bundles
-
-cp vimrc.local ~/.vimrc.local
-cp vimrc.bundles.local ~/.vimrc.bundles.local
-
-git config --global core.autocrlf input
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null
-
-$(which vim) -c "PluginInstall!" -c "q" -c "q"
-
-echo "And you're done! Have fun with your new and improved vim!"
